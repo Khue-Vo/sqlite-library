948f3a56d150fdb70693818094a604fb03cd0216
Khue Vo <vo005642@stthomas.edu>
2024-07-19 08:41:47 +0700

    #1 Create library database

--- database.py
+++ database.py
@@ -5,19 +5,19 @@ import os
 
 my_library = 'library.db'
 conn = sqlite3.connect(my_library)
-lib_path = os.path.abspath(my_library)
 conn.close()
 
 class DatabaseHandler:
 
     def __init__(self):
-        print("Initialize database.")
+        print("Initialize database...")
+        self._conn = conn
+        lib_path = os.path.abspath(my_library)
+        print(f"The database is {lib_path}")
+        self._conn.close()
 
-    def init_database(self):
+    def init_table(self):
         try:
-            # Initialize database
-            conn = sqlite3.connect(my_library)
-            print(f"The database is {lib_path}")
             # Initialize tables
             conn.execute('''CREATE TABLE IF NOT EXISTS Genre
                             (Genre_ID INTEGER NOT NULL,
@@ -68,17 +68,51 @@ class DatabaseHandler:
 
 
     def add_genre(self, genre: str):
-        # input SQL command to insert new category
-        print('add_category')
+        try:
+            conn = sqlite3.connect(my_library)
+            print('add_category')
 
-    def add_author(self, author: str):
-        print("Author added successfully.")
+        except sqlite3.Error as e:
+            return e
 
-    def add_book(self, book_name: str, category_id: int):
-        print('add_book')
-
-    def add_user(self, user: str):
-        print("User added successfully.")
-
-    def add_loan(self, loan: str):
-        print("Loan added successfully.")
+    # def remove_genre(self, genreName: str):
+    #     print("Genre removed successfully.")
+    #
+    # def update_genre(self, genreID: int):
+    #     print("Genre updated successfully.")
+    #
+    # def add_author(self, author: str):
+    #     print("Author added successfully.")
+    #
+    # def remove_author(self, authorName: str):
+    #     print("Author removed successfully.")
+    #
+    # def update_author(self, authorID: int):
+    #     print("Author updated successfully.")
+    #
+    # def add_book(self, book_name: str, category_id: int):
+    #     print('add_book')
+    #
+    # def remove_book(self, bookTitle: str):
+    #     print("Book removed successfully.")
+    #
+    # def update_book(self, bookID: int):
+    #     print("Book updated successfully.")
+    #
+    # def add_user(self, user: str):
+    #     print("User added successfully.")
+    #
+    # def remove_user(self, userName: str):
+    #     print("User removed successfully.")
+    #
+    # def update_user(self, userID: int):
+    #     print("User updated successfully.")
+    #
+    # def add_loan(self, loan: str):
+    #     print("Loan added successfully.")
+    #
+    # def remove_loan(self, loanID: int):
+    #     print("Loan removed successfully.")
+    #
+    # def update_loan(self, loanID: int):
+    #     print("Loan updated successfully.")
