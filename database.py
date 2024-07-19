06dcd3ee07c0a6134df03983cbb217ff7c33587a
Khue Vo <vo005642@stthomas.edu>
2024-07-19 09:21:25 +0700

    #2 Create add_genre method

--- database.py
+++ database.py
@@ -3,6 +3,8 @@
 import sqlite3
 import os
 
+from typing import List
+
 my_library = 'library.db'
 conn = sqlite3.connect(my_library)
 conn.close()
@@ -18,20 +20,21 @@ class DatabaseHandler:
 
     def init_table(self):
         try:
+            self._conn = conn
             # Initialize tables
-            conn.execute('''CREATE TABLE IF NOT EXISTS Genre
+            self._conn.execute('''CREATE TABLE IF NOT EXISTS Genre
                             (Genre_ID INTEGER NOT NULL,
                              GenreName TEXT UNIQUE,
                              PRIMARY KEY(Genre_ID AUTOINCREMENT)
                              );''')
-            conn.execute('''CREATE TABLE IF NOT EXISTS Author
+            self._conn.execute('''CREATE TABLE IF NOT EXISTS Author
                             (Author_ID	INTEGER NOT NULL,
                             FirstName	TEXT,
                             LastName	TEXT,
                             Birthday	TEXT,
                             PRIMARY KEY(Author_ID AUTOINCREMENT)
                             );''')
-            conn.execute('''CREATE TABLE IF NOT EXISTS Book
+            self._conn.execute('''CREATE TABLE IF NOT EXISTS Book
                             (Book_ID INTEGER NOT NULL,
                             Title TEXT UNIQUE,
                             Genre_ID INTEGER,
@@ -42,7 +45,7 @@ class DatabaseHandler:
                             FOREIGN KEY(Genre_ID) REFERENCES Genre(Genre_ID),
                             PRIMARY KEY(Book_ID AUTOINCREMENT)
                             );''')
-            conn.execute('''CREATE TABLE IF NOT EXISTS User
+            self._conn.execute('''CREATE TABLE IF NOT EXISTS User
                             (User_ID INTEGER NOT NULL,
                             FirstName TEXT,
                             LastName TEXT,
@@ -51,7 +54,7 @@ class DatabaseHandler:
                             PhoneNumber TEXT UNIQUE,
                             PRIMARY KEY(User_ID AUTOINCREMENT)
                             );''')
-            conn.execute('''CREATE TABLE IF NOT EXISTS Loan
+            self._conn.execute('''CREATE TABLE IF NOT EXISTS Loan
                             (Loan_ID INTEGER NOT NULL,
                             Book_ID INTEGER,
                             User_ID INTEGER,
@@ -61,58 +64,83 @@ class DatabaseHandler:
                             FOREIGN KEY(User_ID) REFERENCES User(User_ID),
                             PRIMARY KEY(Loan_ID AUTOINCREMENT)
                             );''')
+            self._conn.commit()
             print("Tables created successfully.")
-            conn.close()
+            self._conn.close()
         except sqlite3.Error as e:
             return e
 
 
-    def add_genre(self, genre: str):
+    def add_genre(self, genreName: str):
         try:
-            conn = sqlite3.connect(my_library)
-            print('add_category')
-
+            self._conn = conn
+            self._conn.execute('INSERT INTO Genre (GenreName) VALUES (?)', (genreName,))
+            self._conn.commit()
+            self._conn.close()
+            print(f"Genre '{genreName}' added successfully.")
         except sqlite3.Error as e:
             return e
 
     # def remove_genre(self, genreName: str):
-    #     print("Genre removed successfully.")
-    #
+    #     try:
+    #         self._conn = conn
+    #         self._conn.execute('DELETE FROM Genre WHERE GenreName = ?', (genreName,))
+    #         self._conn.commit()
+    #         self._conn.close()
+    #         print(f"Genre '{genreName}' removed successfully.")
+    #     except sqlite3.Error as e:
+    #         return e
+
+
     # def update_genre(self, genreID: int):
     #     print("Genre updated successfully.")
     #
-    # def add_author(self, author: str):
-    #     print("Author added successfully.")
+    # def add_author(self, author: List[str]):
+    #     try:
+    #         self._conn = conn
+    #         self._conn.execute('INSERT INTO Author (FirstName, LastName) VALUES (?,?)', (author[0], author[1],))
+    #         self._conn.commit()
+    #         self._conn.close()
+    #         print(f"Author '{author}' added successfully.")
+    #     except sqlite3.Error as e:
+    #         return e
     #
-    # def remove_author(self, authorName: str):
-    #     print("Author removed successfully.")
-    #
+    # def remove_author(self, author: List[str]):
+    #     try:
+    #         self._conn = conn
+    #         self._conn.execute('DELETE FROM Genre WHERE FirstName = ? AND LastName = ?', (author[0], author[1],))
+    #         self._conn.commit()
+    #         self._conn.close()
+    #         print(f"Author '{author}' removed successfully.")
+    #     except sqlite3.Error as e:
+    #         return e
+
     # def update_author(self, authorID: int):
     #     print("Author updated successfully.")
     #
-    # def add_book(self, book_name: str, category_id: int):
-    #     print('add_book')
-    #
-    # def remove_book(self, bookTitle: str):
-    #     print("Book removed successfully.")
-    #
+    def add_book(self, book_name: str, category_id: int):
+        print('add_book')
+
+    def remove_book(self, bookTitle: str):
+        print("Book removed successfully.")
+
     # def update_book(self, bookID: int):
     #     print("Book updated successfully.")
     #
-    # def add_user(self, user: str):
-    #     print("User added successfully.")
-    #
-    # def remove_user(self, userName: str):
-    #     print("User removed successfully.")
-    #
+    def add_user(self, user: str):
+        print("User added successfully.")
+
+    def remove_user(self, userName: str):
+        print("User removed successfully.")
+
     # def update_user(self, userID: int):
     #     print("User updated successfully.")
     #
-    # def add_loan(self, loan: str):
-    #     print("Loan added successfully.")
-    #
-    # def remove_loan(self, loanID: int):
-    #     print("Loan removed successfully.")
-    #
+    def add_loan(self, loan: str):
+        print("Loan added successfully.")
+
+    def remove_loan(self, loanID: int):
+        print("Loan removed successfully.")
+
     # def update_loan(self, loanID: int):
     #     print("Loan updated successfully.")
